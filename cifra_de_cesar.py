90e2dfc8ab64f24e641a86776eb1c58c1939ecd2
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-05-31 00:33:26 -0300

    Inclusão do loop para percorrer a string.

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -3,3 +3,5 @@ from time import sleep
 mensagem = str(input('Escreva a mesangem que deseja criptografar: '))
 deslocarLetra = int(input('O deslocamento da letra será em quantas posições, '
                 'informe em números inteiros, por favor: '))
+for letra in mensagem:
+    print(letra)
