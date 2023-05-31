4a3edd87523646530c1f892f5f15a24e3fb1da67
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-05-30 23:45:48 -0300

    Solicitação da quantidade de posições o usuário deseja andar para criptografar.

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,3 +1,5 @@
 from time import sleep
 
 mensagem = str(input('Escreva a mesangem que deseja criptografar: '))
+deslocarLetra = int(input('O deslocamento da letra será em quantas posições, '
+                'informe em números inteiros, por favor: '))
