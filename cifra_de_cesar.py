5730329dd565942f054e23123a6a87b1e16ce0ad
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-01 16:26:02 -0300

    Implementação da lógica da Cifra de César.

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,11 +1,22 @@
 from time import sleep
-
+mensagemCifrada = ''
 mensagem = str(input('Escreva a mesangem que deseja criptografar: '))
 deslocarLetra = int(input('O deslocamento da letra será em quantas posições, '
                 'informe em números inteiros, por favor: '))
 for letra in mensagem:
-    if:
-        if:
+    if letra.isalpha():        
+        if letra.isupper():
+            codigo = ord(letra) - ord('A')
+            codigoNovo = (codigo + deslocarLetra) % 26
+            caractereNovo = chr(codigoNovo + ord('A'))
+            mensagemCifrada += caractereNovo
         else:
+            codigo = ord(letra) - ord('a')
+            codigoNovo = (codigo + deslocarLetra) % 26
+            caractereNovo = chr(codigoNovo + ord('a'))
+            mensagemCifrada += caractereNovo
     else:
+        mensagemCifrada += letra
+
+print('Fim!!!')
 
