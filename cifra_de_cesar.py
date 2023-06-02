04a12b30b525976467688ab633e747676463c3a0
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-01 23:40:26 -0300

    Implementaçao da lógica completa, criação da função cifraDeCesar, impressao da mensagem criptografada.

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,22 +1,30 @@
 from time import sleep
-mensagemCifrada = ''
-mensagem = str(input('Escreva a mesangem que deseja criptografar: '))
-deslocarLetra = int(input('O deslocamento da letra será em quantas posições, '
-                'informe em números inteiros, por favor: '))
-for letra in mensagem:
-    if letra.isalpha():        
-        if letra.isupper():
-            codigo = ord(letra) - ord('A')
-            codigoNovo = (codigo + deslocarLetra) % 26
-            caractereNovo = chr(codigoNovo + ord('A'))
-            mensagemCifrada += caractereNovo
-        else:
-            codigo = ord(letra) - ord('a')
-            codigoNovo = (codigo + deslocarLetra) % 26
-            caractereNovo = chr(codigoNovo + ord('a'))
-            mensagemCifrada += caractereNovo
-    else:
-        mensagemCifrada += letra
 
+def cidraDeCesar():
+        
+    mensagemCifrada = ''
+    mensagem = str(input('Escreva a mesangem que deseja criptografar: '))
+    deslocarLetra = int(input('O deslocamento da letra será em quantas posições, '
+                    'informe em números inteiros, por favor: '))
+    for letra in mensagem:
+        if letra.isalpha():        
+            if letra.isupper():
+                codigo = ord(letra) - ord('A')
+                codigoNovo = (codigo + deslocarLetra) % 26
+                caractereNovo = chr(codigoNovo + ord('A'))
+                mensagemCifrada += caractereNovo
+            else:
+                codigo = ord(letra) - ord('a')
+                codigoNovo = (codigo + deslocarLetra) % 26
+                caractereNovo = chr(codigoNovo + ord('a'))
+                mensagemCifrada += caractereNovo
+        else:
+            mensagemCifrada += letra
+
+    return mensagemCifrada
+
+mensagemCifrada = cidraDeCesar()
+print('Mensagem Cifrada:', mensagemCifrada)
 print('Fim!!!')
+
 
