c53cd7906f02a3419d4a7b7aaf6d0c77ee31f932
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-04 00:26:31 -0300

    Inicialização do arquivo, cifra_de_cesar_negativo.

---
+++ cifra_de_cesar_negativo.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+def cifra_de_cesar_negativo():
+    mensagem = str(input('Digite a mensagem que voce deseja criptografar: '))
+    sleep(1.5)
+    print('Salvando a mensagem digitada, aguarde...')
+    sleep(3.5)
+    
+
