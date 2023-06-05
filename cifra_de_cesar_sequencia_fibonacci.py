191c8526ef189ccce4cbe05cb769ec38c8157e38
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-04 21:46:15 -0300

    criação da função cifra de cesar e seus componentes, laço for e condicionais.

--- cifra_de_cesar_sequencia_fibonacci.py
+++ cifra_de_cesar_sequencia_fibonacci.py
@@ -7,4 +7,27 @@ def sequenciaDeFibonacci(n):
         sequenciaFibonacci.append(proximoNumero)
     return sequenciaFibonacci
     
+def cifraDeCesar():
+    mensagemCifrada = ''
+    mensagem = str(input('Digite a mensagem que voce deseja criptografar: '))
+    sleep(1.5)
+    print('Salvando a mensagem que você digitou, aguarde...')
+    sleep(3.5)
+    
+    deslocarLetra = sequenciaDeFibonacci(len(mensagem))
 
+    for i, letra in enumerate(mensagem):
+        if letra.salpha():
+            if letra.isupper():
+                codigo = ord(letra) - ord('A')
+                codigoNovo = (codigo + deslocarLetra[i]) % 26
+                caractereNovo = chr(codigoNovo + ord('A'))
+                mensagemCifrada += caractereNovo
+            else:
+                codigo = ord(letra) - ord('a')
+                codigoNovo = chr(codigoNovo + ord('a'))
+                mensagemCifrada += caractereNovo
+        else:
+            mensagemCifrada += letra
+    
+    return mensagemCifrada
