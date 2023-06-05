d196f0fa167c8e14b99833bd9364c09b86b66b75
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-04 22:08:57 -0300

    alterações necessarias nas funções, inclusao do loop while onde temos a saída esperada pelo usuário.

--- cifra_de_cesar_sequencia_fibonacci.py
+++ cifra_de_cesar_sequencia_fibonacci.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 def sequenciaDeFibonacci(n):
-    sequenciaFibonacci = [0,1]
+    sequenciaFibonacci = [0, 1]
     while len(sequenciaFibonacci) < n:
         proximoNumero = sequenciaFibonacci[-1] + sequenciaFibonacci[-2]
         sequenciaFibonacci.append(proximoNumero)
@@ -9,7 +9,7 @@ def sequenciaDeFibonacci(n):
     
 def cifraDeCesar():
     mensagemCifrada = ''
-    mensagem = str(input('Digite a mensagem que voce deseja criptografar: '))
+    mensagem = str(input('Digite a mensagem que você deseja criptografar: '))
     sleep(1.5)
     print('Salvando a mensagem que você digitou, aguarde...')
     sleep(3.5)
@@ -17,7 +17,7 @@ def cifraDeCesar():
     deslocarLetra = sequenciaDeFibonacci(len(mensagem))
 
     for i, letra in enumerate(mensagem):
-        if letra.salpha():
+        if letra.isalpha():
             if letra.isupper():
                 codigo = ord(letra) - ord('A')
                 codigoNovo = (codigo + deslocarLetra[i]) % 26
@@ -25,9 +25,28 @@ def cifraDeCesar():
                 mensagemCifrada += caractereNovo
             else:
                 codigo = ord(letra) - ord('a')
-                codigoNovo = chr(codigoNovo + ord('a'))
+                codigoNovo = (codigo + deslocarLetra[i]) % 26
+                caractereNovo = chr(codigoNovo + ord('a'))
                 mensagemCifrada += caractereNovo
         else:
             mensagemCifrada += letra
     
     return mensagemCifrada
+
+while True:
+    print('Aguarde um momento...carregando informações.')
+    sleep(2)
+    mensagemCifrada = cifraDeCesar()
+    print('Criptografando sua mensagem com base na Sequência de Fibonacci.')
+    sleep(1.1)
+    print('Aguarde um momento.')
+    sleep(3.5)
+    print('Mensagem Cifrada:', mensagemCifrada)
+    sleep(2)
+
+    resposta = input('Você deseja cifrar uma nova mensagem? Digite "s" ou "n": ')
+    if resposta.lower() != 's':
+        print('Obrigado!!')
+        sleep(1)
+        break
+print('Fim!!')
